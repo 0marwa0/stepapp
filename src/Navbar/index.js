3f320d36468235c0f241e08e3cd51467965450f4
marwa jawad <marwajawad19@gmail.com>
2020-08-03 05:42:25 +0300

    adding blur effect to the overlay background model

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,18 +1,20 @@
+/** @format */
+
 import React from "react";
 import "./index.css";
 import { FaRegUserCircle } from "react-icons/fa";
 const index = () => {
   return (
-    <div className="navBar">
+    <div className='navBar'>
       <div>
         <img
           src={require("../shared/Icon/Logo.png")}
-          alt=""
+          alt=''
           style={{ height: "22px" }}
         />
       </div>
       <div>
-        <FaRegUserCircle color="white" size="31px" />
+        <FaRegUserCircle color='white' size='24px' />
         {/* <img src={require("../shared/Icon/User.png")} /> */}
       </div>
     </div>
